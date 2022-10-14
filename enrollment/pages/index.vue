04c7a40f667db34b7c34cddf932e540fd3f2da36
iansubing <iansubingsubing@gmail.com>
2022-10-14 11:25:51 +0800

    navigation

--- enrollment/pages/index.vue
+++ enrollment/pages/index.vue
@@ -1,5 +1,21 @@
 <template>
   <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <button class="btn btn-danger navbar-btn">Home</button>
+        </li>
+        <li class="nav-item">
+          <NuxtLink to="/dashboard" class="btn btn-danger navbar-btn">Dashboard</NuxtLink>
+        </li>
+        <li class="nav-item">
+          <button class="btn btn-danger navbar-btn">Register</button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
       <div class="login">
         <div class="login-screen">
           <div class="app-title">
