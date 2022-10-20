4a680cb198d77a2298558a82ec3b05ecc726b34d
iansubing <iansubingsubing@gmail.com>
2022-10-20 18:21:42 +0800

    update

--- enrollment/pages/dashboard.vue
+++ enrollment/pages/dashboard.vue
@@ -1,19 +1,10 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <button class="btn btn-danger navbar-btn">Home</button>
-        </li>
-        <li class="nav-item">
-          <NuxtLink to="/dashboard" class="btn btn-danger navbar-btn">Dashboard</NuxtLink>
-        </li>
-        <li class="nav-item">
-          <button class="btn btn-danger navbar-btn">Register</button>
-        </li>
-      </ul>
-    </div>
-  </nav>
+    <nav>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Courses</a></li>
+      <li><a href="#">About Us</a></li>
+      <li style="float:right"><NuxtLink to="/index" class="btn text-light mh-100">Logout</NuxtLink></li>
+    </nav>
 </template>
 
 <script>
@@ -21,3 +12,34 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style>
+  nav {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #ef3636;
+  }
+
+  li {
+    float: left;
+    border-right:1px solid #bbb;
+  }
+
+  li:last-child {
+    border-right: none;
+  }
+
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+
+  li a:hover:not(.active) {
+    background-color: #111;
+  }
+</style>
